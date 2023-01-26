e0a4b7d28cb49f9cc860cab02b327bc6b48ecb30
Utsav KC <utsavkc789@gmail.com>
2023-01-25 21:39:34 -0600

    updated-cffform-using-materialUI

--- my-app/src/User/Components/CFFForm.js
+++ my-app/src/User/Components/CFFForm.js
@@ -1,5 +1,9 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import Card from "@mui/material/Card";
+import TextField from "@mui/material/TextField";
+import "./CFFForm.css";
+import Button from "@mui/material/Button";
 
 const CFFForm = () => {
   const [OrderDate, setOrderDate] = useState("");
@@ -91,328 +95,55 @@ const CFFForm = () => {
   };
 
   return (
-    // <div className="modalBackground">
-    //   <div className="modalContainer">
-    //     <div className="titleCloseBtn">
-    //       <div className="titleCloseBtn">
-    //         <a
-    //           href="/auth"
-    //           class="btn btn-light"
-    //           role="button"
-    //           aria-pressed="true"
-    //         >
-    //           X
-    //         </a>
-    //       </div>
-    //     </div>
-    //     <form onSubmit={handleSubmit}>
-    //       <div className="row">
-    //         <div className="col-xs-4 col-sm-4 col-md-4">
-    //           <label>
-    //             <b>Order Date</b>
-    //           </label>
-    //           <input
-    //             type="text"
-    //             className="form-control-md"
-    //             required
-    //             min="5"
-    //             onChange={(e) => setOrderDate(e.target.value)}
-    //             value={OrderDate}
-    //           />
-    //         </div>
-    //         <div className="col-xs-7 col-sm-7 col-md-7">
-    //           <label>
-    //             <b>Customer Note</b>
-    //           </label>
-    //           <input
-    //             type="text"
-    //             className="form-control-md"
-    //             onChange={(e) => setCustomerNote(e.target.value)}
-    //             value={CustomerNote}
-    //           />
-    //         </div>
-    //       </div>
-    //       <div className="row">
-    //         <div className="col-xs-4 col-sm-4 col-md-4">
-    //           <label>
-    //             <b>First Name</b>
-    //           </label>
-    //           <input
-    //             type="text"
-    //             className="form-control-md"
-    //             required
-    //             min="3"
-    //             onChange={(e) => setFirstName(e.target.value)}
-    //             value={FirstName}
-    //           />
-    //         </div>
-    //         <div className="col-xs-4 col-sm-4 col-md-4">
-    //           <label>
-    //             <b>Last Name</b>
-    //           </label>
-    //           <input
-    //             type="text"
-    //             className="form-control-md"
-    //             required
-    //             min="3"
-    //             onChange={(e) => setLastName(e.target.value)}
-    //             value={LastName}
-    //           />
-    //         </div>
-    //       </div>
-    //       <div className="row">
-    //         <div className="col-xs-5 col-sm-5 col-md-5">
-    //           <label>
-    //             <b>Dedicated From</b>
-    //           </label>
-    //           <input
-    //             type="text"
-    //             className="form-control-md"
-    //             required
-    //             min="3"
-    //             onChange={(e) => setDedicatedFrom(e.target.value)}
-    //             value={DedicatedFrom}
-    //           />
-    //         </div>
-    //         <div className="col-xs-6 col-sm-6 col-md-6">
-    //           <label>
-    //             <b>Dedicated To</b>
-    //           </label>
-    //           <input
-    //             type="text"
-    //             className="form-control-md"
-    //             required
-    //             min="3"
-    //             onChange={(e) => setDedicatedTo(e.target.value)}
-    //             value={DedicatedTo}
-    //           />
-    //         </div>
-    //       </div>
-    //       <div className="row">
-    //         <div className="col-xs-5 col-sm-5 col-md-5">
-    //           <label>
-    //             <b>Email</b>
-    //           </label>
-    //           <input
-    //             type="text"
-    //             className="form-control-md"
-    //             required
-    //             min="10"
-    //             onChange={(e) => setEmail(e.target.value)}
-    //             value={Email}
-    //           />
-    //         </div>
-    //         <div className="col-xs-4 col-sm-4 col-md-4">
-    //           <label>
-    //             <b>Phone Number</b>
-    //           </label>
-    //           <input
-    //             type="text"
-    //             className="form-control-md"
-    //             required
-    //             min="10"
-    //             onChange={(e) => setPhone(e.target.value)}
-    //             value={Phone}
-    //           />
-    //         </div>
-    //       </div>
-    //       <div className="row">
-    //         <div className="col-xs-4 col-sm-4 col-md-4">
-    //           <label>
-    //             <b>Address</b>
-    //           </label>
-    //           <input
-    //             type="text"
-    //             className="form-control-md"
-    //             required
-    //             min="8"
-    //             onChange={(e) => setAddressBilling(e.target.value)}
-    //             value={AddressBilling}
-    //           />
-    //         </div>
-    //         <div className="col-xs-3 col-sm-3 col-md-3">
-    //           <label>
-    //             <b>City</b>
-    //           </label>
-    //           <input
-    //             type="text"
-    //             className="form-control-md"
-    //             required
-    //             min="3"
-    //             onChange={(e) => setCity(e.target.value)}
-    //             value={City}
-    //           />
-    //         </div>
-    //         <div className="col-xs-2 col-sm-2 col-md-2">
-    //           <label>
-    //             <b>State</b>
-    //           </label>
-    //           <input
-    //             type="text"
-    //             className="form-control-md"
-    //             required
-    //             min="2"
-    //             onChange={(e) => setState(e.target.value)}
-    //             value={State}
-    //           />
-    //         </div>
-    //         <div className="col-xs-2 col-sm-2 col-md-2">
-    //           <label>
-    //             <b>Zip code</b>
-    //           </label>
-    //           <input
-    //             type="text"
-    //             className="form-control-md"
-    //             required
-    //             min="5"
-    //             max="5"
-    //             onChange={(e) => setZip(e.target.value)}
-    //             value={Zip}
-    //           />
-    //         </div>
-    //       </div>
-    //       <div className="row">
-    //         <div className="col-xs-4 col-sm-4 col-md-4">
-    //           <label>
-    //             <b>*Shipping* Address</b>
-    //           </label>
-    //           <input
-    //             type="text"
-    //             className="form-control-md"
-    //             onChange={(e) => setAddressShipping(e.target.value)}
-    //             value={AddressShipping}
-    //           />
-    //         </div>
-    //         <div className="col-xs-3 col-sm-3 col-md-3">
-    //           <label>
-    //             <b>City</b>
-    //           </label>
-    //           <input
-    //             type="text"
-    //             className="form-control-md"
-    //             onChange={(e) => setCityShipping(e.target.value)}
-    //             value={CityShipping}
-    //           />
-    //         </div>
-    //         <div className="col-xs-2 col-sm-2 col-md-2">
-    //           <label>
-    //             <b>State</b>
-    //           </label>
-    //           <input
-    //             type="text"
-    //             className="form-control-md"
-    //             onChange={(e) => setStateShipping(e.target.value)}
-    //             value={StateShipping}
-    //           />
-    //         </div>
-    //         <div className="col-xs-2 col-sm-2 col-md-2">
-    //           <label>
-    //             <b>Zip code</b>
-    //           </label>
-    //           <input
-    //             type="text"
-    //             className="form-control-md"
-    //             onChange={(e) => setZipShipping(e.target.value)}
-    //             value={ZipShipping}
-    //           />
-    //         </div>
-    //       </div>
-    //       <div className="row">
-    //         <div className="col-xs-3 col-sm-3 col-md-3">
-    //           <label>
-    //             <b>Quantity</b>
-    //           </label>
-    //           <input
-    //             type="number"
-    //             className="form-control-md"
-    //             required
-    //             min="1"
-    //             onChange={(e) => setQuantity(e.target.value)}
-    //             value={Quantity}
-    //           />
-    //         </div>
-    //         <div className="col-xs-3 col-sm-3 col-md-3">
-    //           <label>
-    //             <b>Total</b>
-    //           </label>
-    //           <input
-    //             type="number"
-    //             className="form-control-md"
-    //             required
-    //             min="40"
-    //             onChange={(e) => setTotal(e.target.value)}
-    //             value={Total}
-    //           />
-    //         </div>
-    //         <div className="col-xs-3 col-sm-3 col-md-3">
-    //           <label>
-    //             <b>Total items</b>
-    //           </label>
-    //           <input
-    //             type="number"
-    //             className="form-control-md"
-    //             required
-    //             min="1"
-    //             onChange={(e) => setTotalitems(e.target.value)}
-    //             value={Totalitems}
-    //           />
-    //         </div>
-    //       </div>
-    //       <div className="row">
-    //         <div className="col-xs-8 col-sm-8 col-md-8">
-    //           <label>
-    //             <b>Name</b>
-    //           </label>
-    //           <input
-    //             type="text"
-    //             className="form-control-md"
-    //             required
-    //             min="5"
-    //             onChange={(e) => setName(e.target.value)}
-    //             value={Name}
-    //           />
-    //         </div>
-    //       </div>
-    //       <div className="footer">
-    //         <button id="continueBtn">Save</button>
-    //         <Link to="/auth">
-    //           <button id="closeBtn">Close</button>
-    //         </Link>
-    //       </div>
-    //       {Error && <div className="error">{Error}</div>}
-    //     </form>
-    //   </div>
-    // </div>
     <div>
-      <form onSubmit={handleSubmit}>
-        <div className="col-xs-4 col-sm-4 col-md-4">
-          <label>
-            <b>First Name</b>
-          </label>
-          <input
-            type="text"
-            className="form-control-md"
-            required
-            min="3"
-            onChange={(e) => setFirstName(e.target.value)}
-            value={FirstName}
-          />
-        </div>
-        <div className="col-xs-4 col-sm-4 col-md-4">
-          <label>
-            <b>Last Name</b>
-          </label>
-          <input
-            type="text"
-            className="form-control-md"
-            required
-            min="3"
-            onChange={(e) => setLastName(e.target.value)}
-            value={LastName}
-          />
-        </div>
-        <button id="continueBtn">Submit</button>
-      </form>
+      <Card className="card-wrapper">
+        <form className="form-container" onSubmit={handleSubmit}>
+          <div className="col-xs-4 col-sm-4 col-md-4">
+            <TextField
+              className="w-100 text-wrapper"
+              label="First Name"
+              variant="outlined"
+              value={FirstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+            <TextField
+              className="w-100 text-wrapper"
+              label="Last Name"
+              variant="outlined"
+              value={LastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+            <TextField
+              className="w-100 text-wrapper"
+              label="Address Billing"
+              variant="outlined"
+              value={AddressBilling}
+              onChange={(e) => setAddressBilling(e.target.value)}
+            />
+            <TextField
+              className="w-100 text-wrapper"
+              label="City"
+              variant="outlined"
+              value={City}
+              onChange={(e) => setCity(e.target.value)}
+            />
+            <TextField
+              className="w-100 text-wrapper"
+              label="State"
+              variant="outlined"
+              value={State}
+              onChange={(e) => setState(e.target.value)}
+            />
+            <Button
+              className="button-handler"
+              onClick={handleSubmit}
+              variant="contained"
+            >
+              Submit
+            </Button>
+          </div>
+        </form>
+      </Card>
     </div>
   );
 };
