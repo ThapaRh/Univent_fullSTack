6fb56de75768e98d2b8c77ea79231fd6c245f8c4
Utsav KC <utsavkc789@gmail.com>
2023-02-22 11:28:24 -0600

    Made-chages-to-homepage

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,27 +11,28 @@ import Orders from "./Orders/Pages/Orders";
 import UserOrders from "./Orders/Pages/UserOrders";
 import MainNavigation from "./Shared/Components/Navigation/MainNavigation";
 import AllUserTables from "./Table/Pages/AllUserTables";
+import HomePage from "./User/Pages/HomePage";
 
 function App() {
   return (
     <Router>
-    <MainNavigation/>
-    <main>
-      <Switch>
-        <Route path="/" exact>
-          <Users />
-        </Route>
-        <Route path="/usersTable" exact>
-          <AllUserTables></AllUserTables>
-        </Route>
-        <Route path="/:userId/orders" exact>
-        <UserOrders/>
-        </Route>
-        <Route path="/orders">
-          <Orders />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <HomePage />
+          </Route>
+          <Route path="/usersTable" exact>
+            <AllUserTables></AllUserTables>
+          </Route>
+          <Route path="/:userId/orders" exact>
+            <UserOrders />
+          </Route>
+          <Route path="/orders">
+            <Orders />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
       </main>
     </Router>
   );
---
+++ my-app/src/User/Pages/HomePage.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import CarouselExample from "../Components/CarouselExample";
+import NavBar from "../Components/NavBar";
+import Card from "../Components/Card";
+import "./HomePage.css";
+
+const HomePage = () => {
+  const [data, setData] = useState("");
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+      }}
+    >
+      <NavBar />
+      {/* <div style={{ margin: "0 auto" }}>
+        <CarouselExample />
+      </div> */}
+      <img
+        src="https://media.socastsrm.com/wordpress/wp-content/blogs.dir/2798/files/2021/11/georgetown-field-of-honor-georgetown-texas.png"
+        className="imageContainer"
+      />
+
+      <div className="eventWrapper">
+        <p>Event Information : September 3, 2022 - Septemeber 12, 2022</p>
+        <p className="eventItem">3600 WEST ARKANSAS LANE ARLINGTON, TX</p>
+      </div>
+
+      <p className="centerText">
+        Thanks for your interest in volunteering with <br /> <br />
+        The Arlington Field of Honor. <br /> <br />
+        There are many ways you can help during flag assembly, Field of Honor
+        Setup, serving as a docent, Field of Honor take-down.
+      </p>
+
+      <div className="cardContainer">
+        <Card />
+        <Card />
+        <Card />
+      </div>
+      <div>
+        <p className="instHeader"> Instruction on orders </p>
+        <p className="instContent">
+          In order to gain access to all the Orders <br />
+          Please click the order button <br />
+        </p>
+      </div>
+      <p style={{ textAlign: "center" }}>
+        <Button className="orderButton" variant="contained">
+          Orders
+        </Button>
+      </p>
+    </div>
+  );
+};
+
+export default HomePage;
