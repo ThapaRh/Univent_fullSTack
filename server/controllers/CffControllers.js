68d01c8bd9d90a173cb16d42d0029bbe0e4792a0
Utsav KC <utsavkc789@gmail.com>
2023-02-25 12:38:00 -0600

    Parsed Excel file and Uploaded to Database

--- server/controllers/CffControllers.js
+++ server/controllers/CffControllers.js
@@ -2,16 +2,133 @@ const CFF = require("../models/CFFModal");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
-// handle a csv file upload
-const handleFileUpload = (req, res, next) => {
+// HANDLE A FILE UPLOAD
+const handleFileUpload = async (req, res, next) => {
   const fileData = req.body.data;
 
-  console.log(fileData[0]);
+  for (const order of fileData) {
+    const [
+      OrderNumber,
+      OrderStatus,
+      OrderDate,
+      CustomerNote,
+      OrderNote,
+      FirstName,
+      LastName,
+      CompanyBilling,
+      AddressBilling,
+      City,
+      State,
+      ZipCode,
+      Email,
+      Phone,
+      ShippingAddress,
+      ShippingCity,
+      ShippingState,
+      ShippingZip,
+      Item,
+      SKU,
+      ServiceType,
+      Quantity,
+      TotalItems,
+      FormattedDedicatedFrom,
+      FormattedDedicatedTo,
+    ] = [
+      order["Order Number"],
+      order["Order Status"],
+      order["Order Date"],
+      order["Customer Note"],
+      order["Order Notes"],
+      order["First Name"],
+      order["Last Name "],
+      order["Company (Billing)"],
+      order["Address 1 (Billing)"],
+      order["City "],
+      order["State "],
+      order.Zip,
+      order["Email "],
+      order.Phone,
+      order["Address 1 (Shipping)"],
+      order["City (Shipping)"],
+      order["State Code (Shipping)"],
+      order["Zip (Shipping)"],
+      order["Item #"],
+      order.SKU,
+      order.Name,
+      order.Quantity,
+      order["Total items"],
+      order["Dedicated From"],
+      order["Dedicated To | In Memory Of"],
+    ];
+    const DedicatedTo = FormattedDedicatedTo.match(
+      /Flag\s*#\s*1\s*-\s*([\s\S]+)/
+    )[1];
+    const DedicatedFrom = FormattedDedicatedFrom.match(
+      /Flag\s*#\s*1\s*-\s*([\s\S]+)/
+    )[1];
+    const result = await findCFFByOrderNumber(OrderNumber);
+
+    if (!result) {
+      for (let i = 0; i < Quantity; i++) {
+        try {
+          const cff = new CFF({
+            OrderNumber,
+            OrderStatus,
+            OrderDate,
+            CustomerNote,
+            OrderNote,
+            FirstName,
+            LastName,
+            CompanyBilling,
+            AddressBilling,
+            City,
+            State,
+            ZipCode,
+            Email,
+            Phone,
+            ShippingAddress,
+            ShippingCity,
+            ShippingState,
+            ShippingZip,
+            Item,
+            SKU,
+            ServiceType,
+            Quantity,
+            TotalItems,
+            DedicatedFrom,
+            DedicatedTo,
+          });
+          cff.save();
+        } catch (error) {
+          success = false;
+          console.log(error.message);
+        }
+      }
+    }
+  }
 
   res.json({ success: true });
 };
 
-// getting a cff info using id
+// FINDING PREVIOUS CFF BY ORDER NUMBER
+async function findCFFByOrderNumber(OrderNo) {
+  try {
+    const docs = await new Promise((resolve, reject) => {
+      CFF.findOne({ OrderNumber: OrderNo }, function (err, docs) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(docs);
+        }
+      });
+    });
+    return docs;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// GETTING A CFF
 const getCFF = async (req, res, next) => {
   const id = req.params.id;
 
@@ -25,17 +142,32 @@ const getCFF = async (req, res, next) => {
   res.status(200).json({ cff: cff.toObject({ getters: true }) });
 };
 
-// adding a CFF
+// ADDING A CFF
 const createCFF = async (req, res) => {
   const cff = new CFF({
+    OrderNumber: req.body.OrderNumber,
+    OrderStatus: req.body.OrderStatus,
+    OrderDate: req.body.OrderDate,
+    CustomerNote: req.body.CustomerNote,
+    OrderNote: req.body.OrderNote,
     FirstName: req.body.FirstName,
     LastName: req.body.LastName,
     AddressBilling: req.body.AddressBilling,
     City: req.body.City,
     State: req.body.State,
-    Date: req.body.Date,
-    Gender: req.body.Gender,
-    Client_name: req.body.Client_name,
+    Zipcode: req.body.ZipCode,
+    Email: req.body.Email,
+    Phone: req.body.Phone,
+    ShippingAddress: req.body.ShippingAddress,
+    ShippingState: req.body.ShippingState,
+    ShippingZip: req.body.ShippingZip,
+    Item: req.body.Item,
+    SKU: req.body.SKU,
+    ServiceType: req.body.ServiceType,
+    Quantity: req.body.Quantity,
+    TotalItems: req.body.TotalItems,
+    DedicatedFrom: req.body.DedicatedFrom,
+    DedicatedTo: req.body.DedicatedTo,
   });
   try {
     await cff.save();
@@ -48,16 +180,32 @@ const createCFF = async (req, res) => {
 
 // updating a CFF info
 const updateCFF = async (req, res, next) => {
-  const {
-    FirstName,
-    LastName,
-    AddressBilling,
-    City,
-    State,
-    Client_name,
-    Date,
-    Gender,
-  } = req.body;
+  const newCFF = new CFF({
+    OrderNumber: req.body.OrderNumber,
+    OrderStatus: req.body.OrderStatus,
+    OrderDate: req.body.OrderDate,
+    CustomerNote: req.body.CustomerNote,
+    OrderNote: req.body.OrderNote,
+    FirstName: req.body.FirstName,
+    LastName: req.body.LastName,
+    AddressBilling: req.body.AddressBilling,
+    City: req.body.City,
+    State: req.body.State,
+    Zipcode: req.body.ZipCode,
+    Email: req.body.Email,
+    Phone: req.body.Phone,
+    ShippingAddress: req.body.ShippingAddress,
+    ShippingState: req.body.ShippingState,
+    ShippingZip: req.body.ShippingZip,
+    Item: req.body.Item,
+    SKU: req.body.SKU,
+    ServiceType: req.body.ServiceType,
+    Quantity: req.body.Quantity,
+    TotalItems: req.body.TotalItems,
+    DedicatedFrom: req.body.DedicatedFrom,
+    DedicatedTo: req.body.DedicatedTo,
+  });
+
   const id = req.params.id;
 
   let cff;
@@ -68,14 +216,15 @@ const updateCFF = async (req, res, next) => {
     return next(error);
   }
 
-  cff.FirstName = FirstName;
-  cff.LastName = LastName;
-  cff.AddressBilling = AddressBilling;
-  cff.City = City;
-  cff.State = State;
-  cff.Client_name = Client_name;
-  cff.Date = Date;
-  cff.Gender = Gender;
+  // cff.FirstName = FirstName;
+  // cff.LastName = LastName;
+  // cff.AddressBilling = AddressBilling;
+  // cff.City = City;
+  // cff.State = State;
+  // cff.Client_name = Client_name;
+  // cff.Date = Date;
+  // cff.Gender = Gender;
+  cff = newCFF;
 
   try {
     await cff.save();
